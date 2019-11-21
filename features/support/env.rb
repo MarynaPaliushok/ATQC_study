07107f0177e3ce9970972d454c91e5410a5e72f0
Maryna Paliushok <maryna.firsowa@gmail.com>
2019-11-21 12:23:13 +0200

    homework_6

---
+++ features/support/env.rb
@@ -0,0 +1,21 @@
+require 'capybara/cucumber'
+require 'selenium/webdriver'
+require 'site_prism'
+require 'securerandom'
+require 'require_all'
+require 'rest-client'
+
+require_all 'page_objects'
+require_rel '../../modules/feature_helper'
+require_rel '../../modules/api_calls'
+
+def options
+  Selenium::WebDriver::Chrome::Options.new(args: %w[window-size=1800,1000])
+end
+Capybara.default_driver = :selenium
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+World(FeatureHelper)
+World(ApiCalls)
